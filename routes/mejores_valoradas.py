c6a0c396e293a2616689470343db721d38016fff
JuanPabloAriasGarzon <jupaga402@gmail.com>
2025-06-04 23:15:09 -0500

    Mejor Valoracion 2

--- routes/mejores_valoradas.py
+++ routes/mejores_valoradas.py
@@ -1,2 +1,16 @@
+import os
+import requests
+from flask import Blueprint, render_template
+from dotenv import load_dotenv
 
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
+mejores_valoradas_bp = Blueprint("mejores_valoradas", __name__)
+
+@mejores_valoradas_bp.route("/mejores_valoradas")
+def mejores_valoradas():
+    url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=es-ES"
+    response = requests.get(url)
+    datos = response.json()["results"]
+    return render_template("mejores_valoradas.html", datos=datos)
