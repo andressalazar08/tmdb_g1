d450fd2ab5fb66495771c1fe325e12ba2c7b5fe8
krlfgr <karoldhn05@gmail.com>
2025-06-06 19:12:07 -0500

    nuevas actualizaciones

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint
 from routes.popular import popular_bp
+from routes.detalles_una_serie import detalles_una_serie_bp
 import os
 from dotenv import load_dotenv
 
@@ -7,6 +8,7 @@ load_dotenv()
 
 app=Flask(__name__)
 app.register_blueprint(popular_bp)
+app.register_blueprint(detalles_una_serie_bp)
 
 if __name__=="__main__":
     app.run(debug=True)
