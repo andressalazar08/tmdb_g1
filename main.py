0989d3dc55f96c81e02d08106de57a6a95fb0c14
alejotier94 <alejotier@gmail.com>
2025-06-06 20:01:30 -0500

    ruta definida

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, Blueprint
 from routes.popular import popular_bp
-import os
+from routes.modelo import modelo_bp
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app=Flask(__name__)
 app.register_blueprint(popular_bp)
+app.register_blueprint(modelo_bp)
 
 if __name__=="__main__":
     app.run(debug=True)
---
+++ routes/modelo.py
@@ -0,0 +1,18 @@
+import os
+import requests
+from flask import Blueprint, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+modelo_bp = Blueprint("modelo", __name__)
+
+
+
+@modelo_bp.route("/modelo")
+def modelo():
+    url = f"https://api.themoviedb.org/3/movie/now_playing?api_key={API_KEY}&language=es-ES&page=1"
+    response = requests.get(url)
+    datos = response.json()["results"]
+    return render_template("cartelera.html", datos=datos)
