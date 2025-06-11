ae0b4c1362a33bd769840ec5a6959913a02e5280
andres felipe taimal lopez <felipeandres0413@gmal.comgit config --global user.email felipeandres0413@gmal.comgit config --global user.email felipeandres0413@gmal.com>
2025-06-11 18:28:39 -0500

    inicio de aplicacion

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint 
+
 from routes.popular import popular_bp
+
+from routes.genero_de_peliculas import genero_de_peliculas_bp #inporto
+
 import os
+
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
+
 app=Flask(__name__)
+
 app.register_blueprint(popular_bp)
+
+app.register_blueprint(genero_de_peliculas_bp)
+
+
+
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
--- routes/genero_de_peliculas.py
+++ routes/genero_de_peliculas.py
@@ -14,17 +14,14 @@ def genero_de_peliculas():
     print(url)
     response = requests.get(url)
     print(response)
-    id=requests.form.get("id")
+    id=requests.form("id")
+    url={"id"}
     genero_de_peliculas=[
     {"id":15,"name":"Acción"},{"id":12,"name":"Aventura"},{"id":16,"name":"Animación"},{"id":35,"name":"Comedia"},{"id":80,"name":"Crimen"},{"id":99,"name":"Documental"},{"id":18,"name":"Drama"},{"id":10751,"name":"Familia"},{"id":14,"name":"Fantasía"},{"id":36,"name":"Historia"},{"id":27,"name":"Terror"},{"id":10402,"name":"Música"},{"id":9648,"name":"Misterio"},{"id":10749,"name":"Romance"},{"id":878,"name":"Ciencia ficción"},{"id":10770,"name":"Película de TV"},{"id":53,"name":"Suspense"},{"id":10752,"name":"Bélica"},{"id":37,"name":"Western,"}
     ]
-    print(genero_de_peliculas.values())=[]
+    print(genero_de_peliculas.values())
+   
+   
     datos = response.json()["results"]
     print(datos)
-    return render_template("genero_pelicula.html", datos=datos)
-
-    genero_de_peliculas=[
-    {"id":15,"name":"Acción"},{"id":12,"name":"Aventura"},{"id":16,"name":"Animación"},{"id":35,"name":"Comedia"},{"id":80,"name":"Crimen"},{"id":99,"name":"Documental"},{"id":18,"name":"Drama"},{"id":10751,"name":"Familia"},{"id":14,"name":"Fantasía"},{"id":36,"name":"Historia"},{"id":27,"name":"Terror"},{"id":10402,"name":"Música"},{"id":9648,"name":"Misterio"},{"id":10749,"name":"Romance"},{"id":878,"name":"Ciencia ficción"},{"id":10770,"name":"Película de TV"},{"id":53,"name":"Suspense"},{"id":10752,"name":"Bélica"},{"id":37,"name":"Western,"}
-    ]
-
-    print(genero_de_peliculas.values())=[]
+    return render_template("genero_de_peliculas.html", datos=datos)
