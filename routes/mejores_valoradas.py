d8ce138b045b6c76e106c52a3f7a47ec8d393a0a
JuanPabloAriasGarzon <jupaga402@gmail.com>
2025-06-11 20:02:09 -0500

    Mejores Valoradas

--- routes/mejores_valoradas.py
+++ routes/mejores_valoradas.py
@@ -10,7 +10,9 @@ mejoresvaloradas_bp = Blueprint("mejores_valoradas", __name__)
 
 @mejoresvaloradas_bp.route("/mejores_valoradas")
 def mejores_valoradas():
-    url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=es-ES&page=1"
-    response = requests.get(url)
-    datos = response.json()["results"]
+    datos=[]
+    for page in range (1,4):
+        url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=es-ESpage={page}"
+        response = requests.get(url)
+        datos.extend (response.json()["results"])
     return render_template("mejores_valoradas.html", datos=datos)
