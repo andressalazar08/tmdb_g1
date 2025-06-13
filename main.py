eadb86cc6f4258703638f9b5d87b2d21cf7803d6
krlfgr <karoldhn05@gmail.com>
2025-06-13 18:30:59 -0500

    boton terminado

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint
 from routes.popular import popular_bp
-from routes.detalles_una_serie import detalles_una_serie_bp
+from routes.detalles_una_serie import detalles_una_serie_bp, detalles_una_serie_buscar_bp
 import os
 from dotenv import load_dotenv
 
@@ -9,6 +9,7 @@ load_dotenv()
 app=Flask(__name__)
 app.register_blueprint(popular_bp)
 app.register_blueprint(detalles_una_serie_bp)
+app.register_blueprint(detalles_una_serie_buscar_bp)
 
 if __name__=="__main__":
     app.run(debug=True)
