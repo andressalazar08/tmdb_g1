0a3cda784521af950ea41b342ba97a355c6dcd5e
Daniel-LSP <gcinovation12@gmail.com>
2025-06-13 20:03:18 -0500

    estilos 2.0

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from routes.d_series import series_bp
 from routes.mejores_valoradas import mejoresvaloradas_bp
 from routes.popular import popular_bp
 from routes.estrenos import estrenos_bp
-
+from routes.detalles import detalles_bp
 from routes.buscar_pelicula import buscar_bp # importo
 
 
@@ -21,7 +21,7 @@ app.register_blueprint(mejoresvaloradas_bp)
 app.register_blueprint(series_bp)
 app.register_blueprint(popular_bp)
 app.register_blueprint(estrenos_bp)
-
+app.register_blueprint(detalles_bp)
 app.register_blueprint(buscar_bp) # importo la ruta
 
 
