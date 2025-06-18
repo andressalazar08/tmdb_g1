1d7dfed98dfc1de6741f5bd8c9488dfd0c91643f
andres felipe taimal lopez <felipeandres0413@gmal.comgit config --global user.email felipeandres0413@gmal.comgit config --global user.email felipeandres0413@gmal.com>
2025-06-18 18:22:56 -0500

    genero de peliculas parte 2

--- routes/genero_de_peliculas.py
+++ routes/genero_de_peliculas.py
@@ -1,27 +1,28 @@
 import os
 import requests
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
 from dotenv import load_dotenv
+
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 genero_de_peliculas_bp = Blueprint("genero_de_peliculas", __name__)
 
-@genero_de_peliculas_bp.route("/genero_de_peliculas")
+@genero_de_peliculas_bp.route("/genero_de_peliculas",methods={"GET","POST"})
 def genero_de_peliculas():
-    url = f"https://api.themoviedb.org/3/movie/genero_de_peliculas?api_key={API_KEY}&language=es-ES"
+    url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={API_KEY}&language=es-ES"
     print(url)
     response = requests.get(url)
     print(response)
-    id=requests.form("id")
-    url={"id"}
-    genero_de_peliculas=[
-    {"id":15,"name":"Acción"},{"id":12,"name":"Aventura"},{"id":16,"name":"Animación"},{"id":35,"name":"Comedia"},{"id":80,"name":"Crimen"},{"id":99,"name":"Documental"},{"id":18,"name":"Drama"},{"id":10751,"name":"Familia"},{"id":14,"name":"Fantasía"},{"id":36,"name":"Historia"},{"id":27,"name":"Terror"},{"id":10402,"name":"Música"},{"id":9648,"name":"Misterio"},{"id":10749,"name":"Romance"},{"id":878,"name":"Ciencia ficción"},{"id":10770,"name":"Película de TV"},{"id":53,"name":"Suspense"},{"id":10752,"name":"Bélica"},{"id":37,"name":"Western,"}
-    ]
-    print(genero_de_peliculas.values())
+    id=int(request.form.get("id"))
+    print(id)
+    datos = response.json()["genres"]
+    for diccionario in datos:
+        if diccionario["id"]==id:
+            salida=diccionario
+
    
    
-    datos = response.json()["results"]
     print(datos)
-    return render_template("genero_de_peliculas.html", datos=datos)
+    return render_template("genero_de_peliculas.html",salida=salida)
